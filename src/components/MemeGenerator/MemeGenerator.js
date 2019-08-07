43fd53998871fe4589b391b68c1416e84262db50
Alberto Masia <alberto.masia@gmail.com>
2019-08-07 15:58:24 +0200

    Fetch meme images data

--- src/components/MemeGenerator/MemeGenerator.js
+++ src/components/MemeGenerator/MemeGenerator.js
@@ -1,9 +1,26 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 
 class MemeGenerator extends Component {
-  state = {}
+  state = {
+    topText: "",
+    bottomText: "",
+    randomImg: "http://i.imgflip.com/1bij.jpg",
+    allMemeImgs: null
+  }
 
+  componentDidMount() {
+    this.getMemes()
+  }
 
+  getMemes() {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        this.setState({ allMemeImgs: response.data.memes })
+      })
+      .catch(console.error)
+  }
 
   render() {
     return (
