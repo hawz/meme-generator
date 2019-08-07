d1d59d255f2f497748cf7e6883d1c287fbf79cce
Alberto Masia <alberto.masia@gmail.com>
2019-08-07 15:44:13 +0200

    Setup application, create Header and MemeGenerator components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './Header.module.css'
+
+const header = (props) => {
+  return (
+    <header className={classes.Header}>This is the header</header>
+  )
+}
+
+export default header
---
+++ src/components/MemeGenerator/MemeGenerator.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react'
+
+class MemeGenerator extends Component {
+  state = {}
+
+
+
+  render() {
+    return (
+      <div>This is MemeGenerator</div>
+    )
+  }
+}
+
+export default MemeGenerator
