04cdefaaa082388409d014eb80c382fd279685ed
Alberto Masia <alberto.masia@gmail.com>
2019-08-07 15:58:12 +0200

    Style Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,12 @@ import classes from './Header.module.css'
 
 const header = (props) => {
   return (
-    <header className={classes.Header}>This is the header</header>
+    <header className={classes.Header}>
+      <img
+        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+        alt="Problem?"/>
+      <p>Meme Generator</p>
+    </header>
   )
 }
 
